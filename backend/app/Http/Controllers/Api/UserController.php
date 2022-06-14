d363eb8e107a154a5b9c2462d142c1687d1b692e
Kipras-Zelenkovas <80096515+Kipras-Zelenkovas@users.noreply.github.com>
2022-06-14 23:53:29 +0300

    Default CRUD
    Added default user CRUD system.
    Create, Read, Update, Delete user.

---
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response(User::all(), 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return User::create([
+            'name' => $request->get('name'),
+            'surname' => $request->get('surname'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password'))
+        ]) ?
+            response('All ok', 201) :
+            response('Not ok', 400);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return User::where('id', $id)->get();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        $request->get('name') ? $user->name = $request->get('name') : false;
+        $request->get('surname') ? $user->surname = $request->get('surname') : false;
+        $request->get('email') ? $user->email = $request->get('email') : false;
+        $request->get('password') ? $user->password = Hash::make($request->get('password')) : false;
+
+        return $user->save() ?
+            response('Updated', 200) :
+            response('Didn\'t update', 400);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return User::find($id)->delete() ?
+            response('Deleted', 200) :
+            response('Didn\'t delete', 400);
+    }
+}
